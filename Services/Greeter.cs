f25391de85cc913e231b414da6d2477f57d28f8a
Matt Morris <mcm0027@yahoo.com>
2016-04-08 12:34:26 -0500

    add JSON settings file

---
+++ Services/Greeter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting;
+    }
+
+    public class Greeter
+    {
+    }
+}
