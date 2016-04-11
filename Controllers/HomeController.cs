92017adc7bd87877091f55cf175c807c475e8a03
Matt Morris <mcm0027@yahoo.com>
2016-04-10 19:49:07 -0500

    create restaurant entity and view model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.AspNet.Mvc;
-using OdeToFood.Models;
+using OdeToFood.ViewModels;
 using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IGreeter _greeter;
         private IRestaurantData _restaurantData;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(
+            IRestaurantData restaurantData,
+            IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public ViewResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentGreeting = _greeter.GetGreeting();
 
             return View(model);
         }
